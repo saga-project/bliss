3d32ce758cde718b0e9b393271b2c16521ce1633
Ole Weidner <ole.weidner@me.com>
2012-01-02 19:21:32 -0600

    update

--- examples/advanced/bigjob/bigjob_service.py
+++ examples/advanced/bigjob/bigjob_service.py
@@ -2,15 +2,14 @@
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
-'''This examples shows how to start a simple bigjob
-   service that can be used as job submission endpoint
-   via the bliss bigjob plug-in.
+'''This is a prototype for a BigJob service using XML-RPC
+   over HTTP. It encapsulates the subjob API of BigJob and
+   allows to submit jobs to BigJob from a different process
+   or even from a remote machine. 
 
-   Adopted from: http://bit.ly/ujW3XG 
-
-   If something doesn't work as expected, try to set 
-   SAGA_VERBOSE=3 in your environment before you run the
-   script in order to get some debug output.
+   The BigJob client is implemented as a Bliss plugin for 
+   the job and sd APIs. Please check out the file 
+   bigjob_simple.py for example usage.
 
    If you think you have encountered a defect, please 
    report it at: https://github.com/oweidner/bliss/issues
@@ -21,11 +20,51 @@ __email__     = "ole.weidner@me.com"
 __copyright__ = "Copyright 2011, Ole Christian Weidner"
 __license__   = "MIT"
 
-import SocketServer
-from bigjob.bigjob_manager import bigjob
+import SimpleXMLRPCServer
+#from bigjob.bigjob_manager import bigjob
 
-def main():
+class BigJobService(object):
+    '''Encapsulates a BigJob Service'''
+    def __init__(self):
+        '''Constructor'''
+
+    def bigjob_shutdown(self):
+        '''Shut down this service'''
+
+    def bigjob_status(self):
+        '''Get the current status of the BigJob service'''
+
+    def pilot_job_start(self):
+        '''Start a pilot job'''
+
+    def pilot_job_stop(self):
+        '''Stop the currently running pilot job'''
+
+    def pilot_job_state(self):
+        '''Get the state of the currently running pilot job'''
+
+    def subjob_add(self):
+        '''Add a work unit'''
+
+    def subjob_list(self):
+        '''List all subjobs'''
+
+    def subjob_state(self, subjob_id):
+        '''Get the state of a subjob'''
     
+
+
+def run_bigjob_service(hostname="localhost", port="8000"):
+   
+    try: 
+        server = SimpleXMLRPCServer.SimpleXMLRPCServer(("localhost", 8000))
+        server.register_instance(BigJobService())
+        server.serve_forever()
+    except Exception, ex:
+        print "BigJob Service terminated: %s" % str(ex)
+
+
+ 
     COORDINATION_URL    = "advert://advert.cct.lsu.edu:8080/"
     lrms_url            = "fork://localhost"
     workingdirectory    = os.path.join(os.getcwd(), "agent") 
