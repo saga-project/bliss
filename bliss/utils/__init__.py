8445204b17cda5b9ba88b69e50630476a8aa0af8
Ole Weidner <ole.weidner@me.com>
2012-05-28 23:28:46 -0500

    logging utils

--- bliss/utils/__init__.py
+++ bliss/utils/__init__.py
@@ -5,4 +5,4 @@ __author__    = "Ole Christian Weidner"
 __copyright__ = "Copyright 2012, Ole Christian Weidner"
 __license__   = "MIT"
 
-
+from logger import *
---
+++ bliss/utils/logger.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+__author__    = "Ole Christian Weidner"
+__copyright__ = "Copyright 2012, Ole Christian Weidner"
+__license__   = "MIT"
+
+import logging
+
+def get_logger(): 
+    """Return the Bliss logging handler.
+    """
+    l = logging.getLogger("bliss") 
+    return l 
+
+def log_to_file(filename, level=logging.DEBUG): 
+    """Send Bliss logs to a logfile.
+    """ 
+    l = logging.getLogger("bliss") 
+    if len(l.handlers) > 0: 
+        return 
+    l.setLevel(level) 
+    f = open(filename, 'w') 
+    lh = logging.StreamHandler(f) 
+    lh.setFormatter(logging.Formatter('%(levelname)-.3s [%(asctime)s.%(msecs)03d] thr=%(_threadid)-3d %(name)s: %(message)s', 
+                                      '%Y%m%d-%H:%M:%S')) 
+    l.addHandler(lh) 
+
