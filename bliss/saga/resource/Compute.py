052a4984619969938ae7f74b9a0651ac25109dc7
Ole Weidner <ole.weidner@me.com>
2012-06-07 15:41:04 -0500

    import fix

--- bliss/saga/resource/Compute.py
+++ bliss/saga/resource/Compute.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
 __author__    = "Ole Christian Weidner"
 __copyright__ = "Copyright 2012, Ole Christian Weidner"
 __license__   = "MIT"
 
+import bliss.saga.Exception
 from bliss.saga import Url
 from bliss.saga.Object import Object 
 
