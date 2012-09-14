78a81feef6e044396098d520f0d791ce7c748f96
Andre Merzky <andre@merzky.net>
2012-09-14 13:32:46 +0200

    markup fix

--- bliss/saga/__init__.py
+++ bliss/saga/__init__.py
@@ -8,14 +8,14 @@
     ------------
 
     SAGA-Bliss is a light-weight Python package that implements parts of the U{OGF
-    GFD.90 SAGA<www.gridforum.org/documents/GFD.90.pdf>}
+    GFD.90 SAGA <http://www.gridforum.org/documents/GFD.90.pdf>}
     interface specification and provides plug-ins for different
     distributed middleware systems and services. SAGA-Bliss implements the most
     commonly used features of GFD.90 based upon extensive use-case analysis, and
     focuses on usability and simple deployment in real-world heterogeneous
     distributed computing environments and application scenarios.
    
-    More information can be found at: U{http://saga-project.github.com/bliss}
+    More information can be found at http://saga-project.github.com/bliss
 
     Saga Core API Classes
     ---------------------
