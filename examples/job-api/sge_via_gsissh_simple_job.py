bc1352323f76082c12623188e7172ec153a0a7a4
Ole Weidner <ole.weidner@rutgers.edu>
2012-10-19 10:36:04 -0400

    added some more simple examples

--- examples/job-api/sge_via_gsissh_simple_job.py
+++ examples/job-api/sge_via_gsissh_simple_job.py
@@ -2,11 +2,11 @@
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
-'''This examples shows how to submit a job to a PBS 
-   jobmanager via ssh using a custom security context.
+'''This examples shows how to submit a job to an SGE 
+   jobmanager via GSISSH using a custom security context.
 
    If something doesn't work as expected, try to set 
-   SAGA_VERBOSE=3 in your environment before you run the
+   SAGA_VERBOSE=4 in your environment before you run the
    script in order to get some debug output.
 
    If you think you have encountered a defect, please 
@@ -23,41 +23,39 @@ import bliss.saga as saga
 def main():
     
     try:
-        # Optional:
-        # Set up a security context
-        # if no security context is defined, the SFTP
+        # set up a security context (optional)
+        # if no security context is defined, the PBS
         # plugin will pick up the default set of ssh 
         # credentials of the user, i.e., ~/.ssh/id_rsa
-        #
         ctx = saga.Context()
-        ctx.type = saga.Context.SSH
-        ctx.userid  = 'oweidner1' # like 'ssh username@host ...'
-        #ctx.userkey = '/Users/oweidner/.ssh/rsa_work' # like ssh -i ...'
-   
-        # Optional:  
-        # Append the custom security context to the session
-        session = saga.Session()
-        session.contexts.append(ctx)
-
-        # create a job service for Futuregrid's 'india' PBS cluster
+        ctx.type = saga.Context.X509
+        ctx.userproxy = '/tmp/x509up_u501_ncsa'
+        ctx.userid  = 'tg802352' # like 'ssh username@host ...'
+ 
+        # create a job service for TACC's 'lonestar' SGE cluster
         # and attach the SSH security context to it
-        #js = saga.job.Service("pbs+ssh://india.futuregrid.org")
-        # Alternatively: 
-        # Use custom session 
-        js = saga.job.Service("pbs+ssh://india.futuregrid.org", session=session)
+        js = saga.job.Service("sge+gsissh://ranger.tacc.utexas.edu") # replace with lonestar
+        js.session.contexts.append(ctx)
 
         # describe our job
         jd = saga.job.Description()
+        # project to use
+        jd.project = "TG-MCB090174"
+        # queue name is mandatory for SGE adaptor (?) 
+        jd.queue   = "development"
+
         # resource requirements
-        jd.wall_time_limit = 5 #minutes
-        jd.total_cpu_count = 1
+        jd.wall_time_limit = 5 # minutes
+        #jd.total_cpu_count = 13
+        # the email notification
+        #jd.contact = "oweidner@cct.lsu.edu" 
         # environment, executable & arguments
-        jd.environment = {'HELLO':"\"Hello SAGA\""}       
-        jd.executable  = '/bin/echo'
-        jd.arguments   = ['$HELLO']
+        jd.environment = {'SLEEP_TIME':'10'}       
+        jd.executable  = '/bin/sleep'
+        jd.arguments   = ['$SLEEP_TIME']
         # output options
-        jd.output = "bliss_pbssh_job.stdout"
-        jd.error  = "bliss_pbssh_job.stderr"
+        jd.output = "bliss_sgessh_job.stdout"
+        jd.error  = "bliss_sgessh_job.stderr"
 
         # create the job (state: New)
         myjob = js.create_job(jd)
@@ -85,17 +83,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-## INFO: The PBS script generated behind the scenes by the  
-#       plugin looks like this (SAGA_VERBOSE=6 shows it):
-#       
-#         #!/bin/bash 
-#         #PBS -N bliss_job 
-#         #PBS -V     
-#         #PBS -o bliss_pbssh_job.stdout 
-#         #PBS -e bliss_pbssh_job.stderr 
-#         #PBS -l walltime=0:05:00 
-#         #PBS -v SLEEP_TIME=10, 
-#       
-#         /bin/sleep $SLEEP_TIME 
 
